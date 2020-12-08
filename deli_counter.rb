b1a3b0ace6c78e823404a24af39b5a094f18af22
Alexander Rovang <wolf_vii@yahoo.com>
2020-12-08 14:51:06 -0500

    commit tree c18cca33cdf16a00c52508cb1ce66516b19a5693

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+
+def line(array)
+  if array == []
+    puts "The line is currently empty."
+  else
+    greeting = "The line is currently:"
+    array.each_with_index do |name, index|
+      greeting << " #{index + 1}. #{name}"
+    end
+    puts greeting
+  end
+end
+
+def take_a_number(array, name)
+  array << name
+  place = array.length
+  puts "Welcome, #{name}. You are number #{place} in line."
+end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    current = array[0]
+    puts "Currently serving #{current}."
+    array.shift
+  end
+end
